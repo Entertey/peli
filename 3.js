74ec9e8ad8ff90fbb5408a7c73862c8b475742ed
ItsHIM <mikko.anttila04@gmail.com>
2019-10-03 09:23:12 +0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,27 @@
+{
+    "huoneNro": 3,
+    "huoneteksti": "Olet saapunut oikealla olevaan torniin. Tornissa ei näytä olevan paljon tavaraa",
+    "huonehp": null{
+        "hp": 0,
+        "tekstit": {
+            "vaikutus": "Löysit pihalle. Saat lisää voimaa!",
+            "loppu": "kaikki voimat käytetty"
+        },
+        "toistoLkm": 0
+    },
+    "pohjoinen": null,
+    "ita": null,
+    "etela": null,
+    "lansi": {"huoneeseen": 2, "avain": null},
+    "vastustaja":{
+        "nimi": "Snake",
+        "teksti": "Jokin käärme näyttää löytäneensä tien huoneeseen.",
+        "voittoviesti": "Murhasit viattoman käärme eläimen. Saat lisää kokemuspisteitä.",
+        "tappioviesti": "Hävisit ottelun. Käärme puraisi sinua.",
+        "pisteet": 100,
+        "hp": 10,
+        "voittoraja": 10
+    },
+    "avain": null,
+    "esine": null
+},
