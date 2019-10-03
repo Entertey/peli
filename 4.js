6b552ac234d8fb92141db2ce8630556dc54d1d46
ItsHIM <mikko.anttila04@gmail.com>
2019-10-03 09:57:06 +0300

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,34 @@
+{
+    "huoneNro": 4,
+    "huoneteksti": "Saavut salaiseen huoneeseen, tässä huoneessa ei näytä olevan mitään muuta kuin arkku",
+    "huonehp": null{
+        "hp": -5,
+        "tekstit": {
+            "vaikutus": "Yrittäessäsi avata arkkua se puraisee sinua.",
+            "loppu": "kaikki voimat käytetty"
+        },
+        "toistoLkm": 1
+    },
+    "pohjoinen": null,
+    "ita": null,
+    "etela": {"huoneeseen": 3, "avain": null},
+    "lansi": null,
+    "vastustaja":{
+        "nimi": "Mimic",
+        "teksti": "Ehkä sen sisällä on jotain?",
+        "voittoviesti": "Tappaessasi tämän hirvien löydät sen sisuksista avaimen. Hyi.",
+        "tappioviesti": "Sinusta tuli mimicin ruokaa (jos ei ollut selvä: kuolit)",
+        "pisteet": 150,
+        "hp": 20,
+        "voittoraja": 20
+    },
+    "avain": {
+"nimi":"avain",
+"numero": 2,
+"teksti": "Hopean värinen, vai onko se luusta tehty?",
+"vaikutus": "Avain näyttäsi ehkä käyvän rappusten päässä olevaan käytävään",
+"pisteet": 100,
+"hp": 0
+  },
+    "esine": null
+},
