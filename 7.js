199698cb4227fd4612ba5341dc1801a0a644ad5e
playeguy23 <maa1kivi@gmail.com>
2019-10-03 10:18:55 +0300

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,22 @@
+const taso = [
+    {
+        "huoneNro": 7,
+        "huoneteksti": "Olet Viimeisessä huoneessa. Et pääse takaisin. Vastassasi on heikko käärme.",
+        "huonehp": null,
+        "pohjoinen": null,
+        "ita": {"huoneeseen": 8, "avain": null},
+        "etela": null,
+        "lansi": {"huoneeseen": 9, "avain": null},
+        "vastustaja": {
+            "nimi": "Helppo käärme",
+            "teksti": "Käärme on kuoleman partaalla.",
+            "tappoviesti": "Omg Helpoin vastustaja ja hävisit!",
+            "voittoviesti": "Hyvä voitit helpoimman vastuksen. Vasemmalla on pimeä, mutta kiinnostava huone. Oikealla on valoisa ja vapaan oloinen huone. Kumman valitset?",
+            "pisteet": 0,
+            "hp": 4,
+            "voittoraja": 99
+        },
+        "avain": null,
+        "esine": null
+    },
+]
