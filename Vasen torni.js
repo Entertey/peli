2f278123c5f6508d18c5c441944487581ffbeec5
Odlivion <56020911+Odlivion@users.noreply.github.com>
2019-10-03 09:33:10 +0300

    Add files via upload

---
+++ Vasen torni.js
@@ -0,0 +1,20 @@
+"huoneet": [
+    {
+        "huoneNro": 1,
+        "huoneteksti": "Olet Korkeassa tornissa ja huomaat kuinka hieno näkymä on.",
+        "huonehp": {
+            "hp": 0,
+            "tekstit": {
+                "vaikutus": "Löysit torniin.",
+                "loppu": "kaikki voimat käytetty"
+            },
+            "toistoLkm": 2
+        },
+        "pohjoinen": {"huoneeseen": 6, "avain": null},
+        "ita": {"huoneeseen": 2, "avain": null},
+        "etela": null,
+        "lansi": null,
+        "vastustaja": null,
+        "avain": null,
+        "esine": null
+    },
