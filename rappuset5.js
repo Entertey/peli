eae96a762c1febcba0aa454f8dd2c0e8d504b994
playeguy23 <maa1kivi@gmail.com>
2019-10-03 09:38:08 +0300

    Add files via upload

---
+++ rappuset5.js
@@ -0,0 +1,21 @@
+const taso = [
+    {
+        "huoneNro": 5,
+        "huoneteksti": "Olet Rappusissa. Eteläseinässä on kultaisena kiiltävä raskas teräs ovi. Idässä oleva ovi on hento ja heiveröinen ovi.",
+        "huonehp": {
+            "hp": 10, 
+            "tekstit": {
+                "vaikutus":"Huone on täynnä pölyä. Pölyn näkeminen piristää kummasti.", 
+                "loppu": "Pöly loppuu aikanaan"
+            }, 
+            "toistoLkm": 1
+        },
+        "pohjoinen": null,
+        "ita": null,
+        "etela": {"huoneeseen": 4, "avain": null},
+        "lansi": {"huoneeseen": 7, "avain": 2},
+        "vastustaja": null,
+        "avain": null,
+        "esine": null  
+    }
+]
